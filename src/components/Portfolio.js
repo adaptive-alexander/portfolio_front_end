65cb9993e542b1ce1bcd5bff7ddf6e3e143b728e
Alexander Hyll <alexander@adaptivehedge.com>
2022-09-23 19:18:07 +0200

    chore

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,14 +10,15 @@ function Portfolio() {
                 Management, which has now been discontinued. In that role he was responsible for the management of the
                 company and fund, with a primary focus on investment strategy and research.</p>
             <p>The fund used advanced statistical models and big data to find relative out-performance in markets.
-                As such a large portion of time was spent developing statistical models, including machine learning, as
+                As such, a large portion of time was spent developing statistical models, including machine learning, as
                 well as building data pipelines and infrastructure for large analytical workloads. The portfolios repo
                 can be found <a target="_blank" rel="noreferrer"
                                 href="images/frontend/src/components/Portfolio">here</a>.</p>
             <h2>Projects</h2>
             <p>The projects, as well as this website, are run on cloud infrastructure (GCP) powered by Kubernetes. The
-                front-end is a React app, while the back-end is an Actix web server running a GraphQL API, with
-                computations handled by containerized Rust and Python.</p>
+                front-end is a React app, while the back-end is an Actix web server running a REST and GraphQL API, with
+                computations handled by containerized Rust and Python. This is of course over-engineered but serves as
+                a better demonstration of different techniques.</p>
         </div>
     );
 }
