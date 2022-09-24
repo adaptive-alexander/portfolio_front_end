5c64cb375f4078156f3bd2bd7c67885419246aef
Alexander Hyll <alexander@adaptivehedge.com>
2022-09-24 04:10:07 +0200

    linebreak

--- src/components/Headshot.js
+++ src/components/Headshot.js
@@ -7,10 +7,10 @@ function Headshot() {
             <img src="./assets/headshot.jpg" alt="Alexander" className="Headshot"/>
             <h3>Alexander Hyll</h3>
             <p>Passionate about fast, safe, well-documented code.</p>
-            <a href="mailto:alexander.hyll@gmail.com">alexander.hyll@gmail.com</a>
+            <a href="mailto:alexander.hyll@gmail.com">alexander.hyll@gmail.com</a> <br />
             <img src="./assets/GitHub-Mark-Light-32px.png" alt="logo" className="Logos" />    <a href="github.com/adaptive-alexander">GitHub</a> <br />
             <img src="./assets/LinkedIn.png" alt="logo" className="Logos" />    <a href="https://linkedin.com/alexander-hyll">LinkedIn</a> <br />
-            <a href="127.0.0.1:8080">CV</a>
+            <a href="">CV</a>
         </div>
     );
 }
